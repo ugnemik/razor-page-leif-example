821899e73846cb4b8a40014e1b821447b0d0bcac
Jon Maskrey <j.maskrey@zengenti.com>
2021-12-16 11:46:31 +0000

    Add model

---
+++ RazorPageLeifExample/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+using Zengenti.Contensis.Delivery;
+
+namespace RazorPageLeifExample.Models {
+    public class BlogPost {
+        public string? Title { get; set; }
+        public string? LeadParagraph { get; set; }
+        public Image? ThumbnailImage { get; set; }
+        public Person? Author { get; set; }
+        public ComposedField? PostBody { get; set; }
+    }
+
+    public class Person  {
+        public string? Name { get; set; }
+        public Image? Photo { get; set; }
+    }
+}
