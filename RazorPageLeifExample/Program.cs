e590d5d275491cedd272e228a54c42ab120aef9e
Neil Flatley <n.flatley@zengenti.com>
2022-11-22 21:51:39 +0000

    chore: set up omnisharp for net6.0 and cs linting, use http logging middleware

--- RazorPageLeifExample/Program.cs
+++ RazorPageLeifExample/Program.cs
@@ -1,4 +1,5 @@
 using Zengenti.Contensis.Delivery;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,6 +19,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseHttpLogging();
 
 app.UseAuthorization();
 
@@ -26,11 +28,13 @@ app.UseStatusCodePagesWithReExecute("/Error");
 app.MapRazorPages();
 
 // Contensis client connection details
-ContensisClient.Configure(new ContensisClientConfiguration(
-    rootUrl: "https://cms-leif.cloud.contensis.com",
-    projectApiId: "website",
-    clientId: "2f3165ff-7841-4e7d-83c6-79770275bbe1",
-    sharedSecret: "23df7fdd8744496c91a9a3fd7d7cf16f-4908e4643eda4d6989f07f806ead697b-447133b6b55d4767a5a0bb47cebb87a2"
-));
+ContensisClient.Configure(
+    new ContensisClientConfiguration(
+        rootUrl: "https://cms-leif.cloud.contensis.com",
+        projectApiId: "website",
+        clientId: "2f3165ff-7841-4e7d-83c6-79770275bbe1",
+        sharedSecret: "23df7fdd8744496c91a9a3fd7d7cf16f-4908e4643eda4d6989f07f806ead697b-447133b6b55d4767a5a0bb47cebb87a2"
+    )
+);
 
 app.Run();
