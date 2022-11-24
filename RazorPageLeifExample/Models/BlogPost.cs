2a6d9b34862ea948cb3a1b35ab7c1310859284ae
Simon Horan <s.horan@zengenti.com>
2022-11-24 12:49:09 +0000

    Cahnge thumbnail image to primary image

--- RazorPageLeifExample/Models/BlogPost.cs
+++ RazorPageLeifExample/Models/BlogPost.cs
@@ -1,11 +1,13 @@
 using Zengenti.Contensis.Delivery;
 
-namespace RazorPageLeifExample.Models {
-    public class BlogPost: EntryModel {
+namespace RazorPageLeifExample.Models
+{
+    public class BlogPost : EntryModel
+    {
         public string Title { get; set; } = null!;
         public string? LeadParagraph { get; set; }
-        public Image? ThumbnailImage { get; set; }
-        public string? ThumbnailImageUrl => ImageHelper.GetImageUrl(ThumbnailImage);
+        public Image? PrimaryImage { get; set; }
+        public string? PrimaryImageUrl => ImageHelper.GetImageUrl(PrimaryImage);
         public Person? Author => Resolve<Person>("author");
         public Category? Category => Resolve<Category>("category");
         public ComposedField? PostBody { get; set; }
